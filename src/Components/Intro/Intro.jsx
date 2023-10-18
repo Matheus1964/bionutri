46bb5baa17f3caf1f2d2844b5d5e1036f8435fd0
GabrielSanic <ghenrique018@gmail.com>
2023-10-18 14:48:20 -0300

    Layout Match Perfeito

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,23 +1,23 @@
-import styles from "./Intro.module.css"
+import introstyles from "./Intro.module.css"
 import Banner from "../../Assets/Home/intro-banner.png"
 import { Link } from "react-router-dom"
 
 function Intro(){
   return(
-    <div class={styles.container}>
-      <div class={styles.descricao}>
+    <div class={introstyles.container}>
+      <div class={introstyles.descricao}>
         <h2>Software de Nutrição completo com as melhores soluções!</h2>
         <p>Tudo que o Nutricionista precisa para facilitar a rotina, gerir seu consultório e fidelizar pacientes - em um só lugar.</p>
-        <div class={styles.botoes}>
+        <div class={introstyles.botoes}>
           <Link to ="">
-            <button class={styles.consultabtn}>Marque uma Consulta agora mesmo</button>
+            <button class={introstyles.consultabtn}>Marque uma Consulta agora mesmo</button>
           </Link>
           <Link to ="">
-            <button class={styles.acessarbtn}>Acessar</button>
+            <button class={introstyles.acessarbtn}>Acessar</button>
           </Link>
         </div>
       </div>
-      <div class={styles.containerimg}>
+      <div class={introstyles.containerimg}>
         <img src={Banner} alt="Banner"/>
       </div>
     </div>
