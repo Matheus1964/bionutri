3432e0e38f1e6dadaa4e74fee8485326bc643e4a
Taiyo <taiyo@unipam.edu.br>
2023-10-22 17:43:58 -0300

    Criação do layout do IMC e adição a Home

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -2,12 +2,14 @@ import About from "../../Components/About/index"
 import Intro from "../../Components/Intro/Intro"
 import Match from "../../Components/Match/Match"
 import Footer from "../../Components/Footer/Footer"
-
+import ImcLayout from "../../Components/ImcLayout/ImcLayout"
+import React from "react"
 
 function Home (){
   return (
     <div>
       <Intro/>
+      <ImcLayout/>
       <About/>
       <Match/>
       <Footer/>
