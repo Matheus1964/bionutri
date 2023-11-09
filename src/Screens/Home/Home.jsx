83b373ec6bd53fdc01c87ddc571d6ec9be58c5e4
matheus1964 <mateusaragao1@hotmail.com>
2023-11-09 17:52:01 -0300

    teste

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -4,6 +4,7 @@ import Match from "../../Components/Match/Match"
 import Footer from "../../Components/Footer/Footer"
 import ImcLayout from "../../Components/ImcLayout/ImcLayout"
 import Header from "../../Components/Header/Header"
+
 import React from "react"
 
 function Home (){
