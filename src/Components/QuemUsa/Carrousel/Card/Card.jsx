05ef6a0f3c6b45a67123d205be53372570c9fdc5
GabrielSanic <ghenrique018@gmail.com>
2023-11-12 15:54:50 -0300

    tamanho carrousel diminuido

--- src/Components/QuemUsa/Carrousel/Card/Card.jsx
+++ src/Components/QuemUsa/Carrousel/Card/Card.jsx
@@ -6,11 +6,13 @@ function Card() {
   return (
    <>
    <div className={Styles.container}>
+    <div className={Styles.wrapper}>
     <div className={Styles.identidade}>
         <img className={Styles.imagem} src={ProfilePic} alt="Imagem de perfil" />
         <h3 className={Styles.nome}>Nome</h3>
     </div>
     <p className={Styles.descricao}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, quisquam commodi! Illo repellendus a exercitationem distinctio quae reprehenderit temporibus officia ducimus labore dignissimos sint, dolorem quis ea necessitatibus iure voluptate sed tenetur! Nostrum minima laborum provident facere voluptas error consequatur?</p>
+    </div>
    </div>
    </>
   );
